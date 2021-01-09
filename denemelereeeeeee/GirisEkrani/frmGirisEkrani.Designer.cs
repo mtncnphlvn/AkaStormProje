42a9e63d98c6a670d76235a66427e053247dba4b
GreatStorm <storm3453@icloud.com>
2021-01-09 17:57:48 +0300

    GirişEkranı

--- denemelereeeeeee/GirisEkrani/frmGirisEkrani.Designer.cs
+++ denemelereeeeeee/GirisEkrani/frmGirisEkrani.Designer.cs
@@ -29,48 +29,128 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFGiris = new System.Windows.Forms.Button();
+            this.btnFKayit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(87, 26);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.label1.Location = new System.Drawing.Point(79, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 47);
+            this.label1.Size = new System.Drawing.Size(161, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı";
             // 
-            // label2
+            // btnGiris
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Print", 20F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(414, 26);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 47);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Geliştirici";
+            this.btnGiris.BackgroundImage = global::AkaStormProje.Properties.Resources.BtnLeave;
+            this.btnGiris.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGiris.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnGiris.FlatAppearance.BorderSize = 0;
+            this.btnGiris.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnGiris.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGiris.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.btnGiris.Location = new System.Drawing.Point(93, 90);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(132, 64);
+            this.btnGiris.TabIndex = 3;
+            this.btnGiris.Text = "GİRİŞ";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.MouseLeave += new System.EventHandler(this.btnGiris_MouseLeave);
+            this.btnGiris.MouseHover += new System.EventHandler(this.btnGiris_MouseHover);
             // 
-            // button1
+            // btnKayit
             // 
-            this.button1.Location = new System.Drawing.Point(459, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Kayıt Ol";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnKayit.BackgroundImage = global::AkaStormProje.Properties.Resources.BtnLeave;
+            this.btnKayit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKayit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnKayit.FlatAppearance.BorderSize = 0;
+            this.btnKayit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnKayit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKayit.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKayit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.btnKayit.Location = new System.Drawing.Point(93, 170);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(132, 64);
+            this.btnKayit.TabIndex = 4;
+            this.btnKayit.Text = "KAYIT OL";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.MouseLeave += new System.EventHandler(this.btnKayit_MouseLeave);
+            this.btnKayit.MouseHover += new System.EventHandler(this.btnKayit_MouseHover);
             // 
-            // button2
+            // label3
             // 
-            this.button2.Location = new System.Drawing.Point(113, 122);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 33);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Giriş";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.label3.Location = new System.Drawing.Point(400, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Geliştirici";
+            // 
+            // btnFGiris
+            // 
+            this.btnFGiris.BackgroundImage = global::AkaStormProje.Properties.Resources.BtnLeave;
+            this.btnFGiris.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFGiris.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFGiris.FlatAppearance.BorderSize = 0;
+            this.btnFGiris.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFGiris.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFGiris.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFGiris.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.btnFGiris.Location = new System.Drawing.Point(423, 90);
+            this.btnFGiris.Name = "btnFGiris";
+            this.btnFGiris.Size = new System.Drawing.Size(132, 64);
+            this.btnFGiris.TabIndex = 6;
+            this.btnFGiris.Text = "GİRİŞ";
+            this.btnFGiris.UseVisualStyleBackColor = true;
+            this.btnFGiris.MouseLeave += new System.EventHandler(this.btnFGiris_MouseLeave);
+            this.btnFGiris.MouseHover += new System.EventHandler(this.btnFGiris_MouseHover);
+            // 
+            // btnFKayit
+            // 
+            this.btnFKayit.BackgroundImage = global::AkaStormProje.Properties.Resources.BtnLeave;
+            this.btnFKayit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFKayit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFKayit.FlatAppearance.BorderSize = 0;
+            this.btnFKayit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFKayit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(104)))), ((int)(((byte)(0)))));
+            this.btnFKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFKayit.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFKayit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(150)))));
+            this.btnFKayit.Location = new System.Drawing.Point(423, 170);
+            this.btnFKayit.Name = "btnFKayit";
+            this.btnFKayit.Size = new System.Drawing.Size(132, 64);
+            this.btnFKayit.TabIndex = 7;
+            this.btnFKayit.Text = "KAYIT OL";
+            this.btnFKayit.UseVisualStyleBackColor = true;
+            this.btnFKayit.MouseLeave += new System.EventHandler(this.btnFKayit_MouseLeave);
+            this.btnFKayit.MouseHover += new System.EventHandler(this.btnFKayit_MouseHover);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImage = global::AkaStormProje.Properties.Resources.çarpı;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Location = new System.Drawing.Point(605, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(33, 29);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmGirisEkrani
             // 
@@ -80,9 +160,12 @@
             this.BackgroundImage = global::AkaStormProje.Properties.Resources.girisEkrani;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(650, 300);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFKayit);
+            this.Controls.Add(this.btnFGiris);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.btnGiris);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmGirisEkrani";
@@ -90,6 +173,9 @@
             this.Tag = "<";
             this.Text = "frmGirisEkrani";
             this.Load += new System.EventHandler(this.frmGirisEkrani_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmGirisEkrani_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmGirisEkrani_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmGirisEkrani_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,8 +184,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnFGiris;
+        private System.Windows.Forms.Button btnFKayit;
+        private System.Windows.Forms.Button btnExit;
     }
 }
--- denemelereeeeeee/GirisEkrani/frmGirisEkrani.cs
+++ denemelereeeeeee/GirisEkrani/frmGirisEkrani.cs
@@ -21,5 +21,73 @@ namespace AkaStormProje
         {
           
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnGiris_MouseHover(object sender, EventArgs e)
+        {
+            btnGiris.BackgroundImage = Properties.Resources.BtnHvr;
+        }
+
+        private void btnGiris_MouseLeave(object sender, EventArgs e)
+        {
+            btnGiris.BackgroundImage = Properties.Resources.BtnLeave;
+        }
+
+        private void btnKayit_MouseHover(object sender, EventArgs e)
+        {
+            btnKayit.BackgroundImage = Properties.Resources.BtnHvr;
+        }
+
+        private void btnKayit_MouseLeave(object sender, EventArgs e)
+        {
+            btnKayit.BackgroundImage = Properties.Resources.BtnLeave;
+        }
+
+        private void btnFGiris_MouseHover(object sender, EventArgs e)
+        {
+            btnFGiris.BackgroundImage = Properties.Resources.BtnHvr;
+        }
+
+        private void btnFGiris_MouseLeave(object sender, EventArgs e)
+        {
+            btnFGiris.BackgroundImage = Properties.Resources.BtnLeave;
+        }
+
+        private void btnFKayit_MouseHover(object sender, EventArgs e)
+        {
+            btnFKayit.BackgroundImage = Properties.Resources.BtnHvr;
+        }
+
+        private void btnFKayit_MouseLeave(object sender, EventArgs e)
+        {
+            btnFKayit.BackgroundImage = Properties.Resources.BtnLeave;
+        }
+
+        bool move;
+        int mouse_x;
+        int mouse_y;
+        private void frmGirisEkrani_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mouse_x = e.X;
+            mouse_y = e.Y;
+        }
+
+        private void frmGirisEkrani_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(move)
+            {
+                this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
+            }
+        }
+
+        private void frmGirisEkrani_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
     }
 }
